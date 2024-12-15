7ce6acc6d0ead6ffde465dce050227d041b3e646
jcasp09 <jcasper@stevens.edu>
2024-12-15 14:38:42 -0500

    gyms/comments route templates

---
+++ routes/comments.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import commentData from '../data/comments.js';
+
+const router = Router();
+
+// Create a new comment
+router.post('/', async (req, res) => {
+    
+});
+
+// Get a comment by objectId
+router.get('/:id', async (req, res) => {
+
+});
+
+// Update a comment using obectId
+router.patch('/:id', async (req, res) => {
+
+});
+
+// Delete a comment by objectId
+router.delete('/:id', async (req, res) => {
+
+});
+
+export default router;
+  
---
+++ routes/gyms.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import gymData from '../data/gyms.js';
+
+const router = Router();
+
+// Create a new gym profile
+router.post('/', async (req, res) => {
+    
+});
+
+// Get a gym by objectId
+router.get('/:id', async (req, res) => {
+
+});
+
+// Update a gym profile using obectId
+router.patch('/:id', async (req, res) => {
+
+});
+
+// Delete a gym profile by objectId
+router.delete('/:id', async (req, res) => {
+
+});
+
+export default router;
+  
