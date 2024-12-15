6419ed9bb4bd76889eb17c3315669e23883ea1fd
jcasp09 <jcasper@stevens.edu>
2024-12-15 15:37:48 -0500

    gyms.js, comment.js data templates

---
+++ data/comments.js
@@ -0,0 +1,17 @@
+import {ObjectId } from 'mongodb';
+import {comments} from '../config/mongoCollections.js';
+
+// Create comment
+export const createCommente = async (comment) => {
+
+}
+
+// Get comment by id
+export const getCommentById = async (commentId) => {
+    
+}
+
+// Delete comment
+export const deleteComment = async (commentId) => {
+    
+}
---
+++ data/gyms.js
@@ -0,0 +1,23 @@
+import {ObjectId } from 'mongodb';
+import bcrypt from 'bcrypt';
+import {gyms} from '../config/mongoCollections.js';
+
+// Create gym
+export const createGym = async (name, password, address, hours, equipment, classes, personal_trainers, extra) => {
+
+}
+
+// Get gym by id
+export const getGymById = async (gymID) => {
+    
+}
+
+// Update gym content
+export const updateGym = async (gymID) => {
+    
+}
+
+// Delete gym
+export const deleteGym = async (gymID) => {
+    
+}
