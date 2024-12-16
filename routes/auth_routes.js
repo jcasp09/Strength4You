81c363f85738b646862c0661e0ed82c55b2cd961
jcasp09 <jcasper@stevens.edu>
2024-12-16 13:43:14 -0500

    removed signin route from auth_routes

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -20,19 +20,6 @@ router.route('/home/search').get(async (req, res) => {
 
 
 
-router
-  .route('/signin')
-  .get(async (req, res) => {
-    // Render Sign In handlebar
-    return res.render('signin')
-  })
-  .post(async (req, res) => {
-    // Submit signin request
-    
-});
-
-
-
 router.route('/signoutuser').get(async (req, res) => {
   // Render Sign out handlebar after destroying user's session
   req.session.destroy()
