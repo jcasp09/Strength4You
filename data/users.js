536d9fb01d0db72533cf8f64b8b33681333f11a2
jcasp09 <jcasper@stevens.edu>
2024-12-16 12:47:51 -0500

    misc

--- data/users.js
+++ data/users.js
@@ -2,65 +2,64 @@ import { ObjectId } from 'mongodb'; // object id validation
 import bcrypt from 'bcryptjs'; // for hashing passwords
 import { users } from '../config/mongoCollections.js'; // user collection from database
 
-const exportedMethods = {
-  // Throws error if userId is already in the database
-  async duplicateUserCheck(userId) {
-    const userCollection = await users()
-    const user = await userCollection.findOne({userId: userId})
-    if (user) throw `Error: there is already a user with that user ID.`
-    return
-  },
 
+// Throws error if userId is already in the database
+const duplicateUserCheck = async (userId) => {
+  const userCollection = await users()
+  const user = await userCollection.findOne({userId: userId})
+  if (user) throw `Error: there is already a user with that user ID.`
+}
 
-  // create new user
-  async createUser(firstName, lastName, userId, password, email, dob, city, state) {
-    // Hash password before saving
-    const hashedPassword = await bcrypt.hash(password, 10);
+// create new user
+export const createUser = async (firstName, lastName, userId, password, email, dob, city, state) => {
+  // Check for user with that userId already in db
+  await duplicateUserCheck(userId)
+  // Hash password before saving
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = {
-      firstName,
-      lastName,
-      userId,
-      password: hashedPassword,
-      email,
-      dob,
-      city, 
-      state
-    };
+  const newUser = {
+    firstName,
+    lastName,
+    userId,
+    password: hashedPassword,
+    email,
+    dob,
+    city, 
+    state
+}
 
-    // Insert user object into database
-    const userCollection = await users();
-    const insertInfo = await userCollection.insertOne(newUser);
-    if (!insertInfo) throw `Error: Could not add user.`
-  },
+  // Insert user object into database
+  const userCollection = await users();
+  const insertInfo = await userCollection.insertOne(newUser);
+  if (!insertInfo) throw `Error: Could not add user.`
+  // User successfully added to db
+  return true
+}
 
-  // get a user by id
-  async getUserById(userId) {
-    const userCollection = await users();
-    const user = await userCollection.findOne({ _id: new ObjectId(userId) });
-    if (!user) return null; // Return null if user not found
+// get a user by id
+export const getUserById = async (userId) => {
+  const userCollection = await users();
+  const user = await userCollection.findOne({ _id: new ObjectId(userId) });
+  if (!user) return null; // Return null if user not found
 
-    user._id = user._id.toString(); // Convert _id to string before returning
-    return user;
-  },
+  user._id = user._id.toString(); // Convert _id to string before returning
+  return user;
+}
 
-  // update user information
-  async updateUser(userId, updatedData) {
-    const userCollection = await users();
-    const updatedUser = await userCollection.findOneAndUpdate(
-      { _id: new ObjectId(userId) },
-      { $set: updatedData },
-      { returnDocument: 'after' }
-    );
-    return updatedUser.value;
-  },
+// update user information
+export const updateUser = async (userId, updatedData) => {
+  const userCollection = await users();
+  const updatedUser = await userCollection.findOneAndUpdate(
+    { _id: new ObjectId(userId) },
+    { $set: updatedData },
+    { returnDocument: 'after' }
+  );
+  return updatedUser.value;
+}
 
-  // delete user by id
-  async deleteUser(userId) {
-    const userCollection = await users();
-    const deletionInfo = await userCollection.deleteOne({ _id: new ObjectId(userId) });
-    return { deleted: deletionInfo.deletedCount > 0 };
-  },
-};
-
-export default exportedMethods;
+// delete user by id
+export const deleteUser = async (userId) => {
+  const userCollection = await users();
+  const deletionInfo = await userCollection.deleteOne({ _id: new ObjectId(userId) });
+  return { deleted: deletionInfo.deletedCount > 0 };
+}
