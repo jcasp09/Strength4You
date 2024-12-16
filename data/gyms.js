2566f4aed6fa59a6d4a597a8692ca7dc2147b1cc
Vincent Cancelliere <vcancell@stevens.edu>
2024-12-15 23:01:27 -0500

    Created gym functions and added more validation functions

--- data/gyms.js
+++ data/gyms.js
@@ -1,23 +1,119 @@
 import {ObjectId } from 'mongodb';
 import bcrypt from 'bcryptjs';
 import {gyms} from '../config/mongoCollections.js';
+import validation from '../validation.js'
+const saltRounds = 10;
 
 // Create gym
-export const createGym = async (name, password, address, hours, equipment, classes, personal_trainers, extra) => {
+export const createGym = async (name, username, password, email, address, hours, equipment, classes, extra, link) => {
+    name = validation.checkString(name)
+    username = validation.checkUser(username);
+    email = validation.checkEmail(email);
+    password = validation.checkPassword(password)
+    address = validation.checkString(address)
+    hours = validation.checkHours(hours)
 
+    let 
+    equipment = [],
+    classes = [],
+    extra = [],
+    link = "",
+    reviews = [],
+    rating = 0,
+    comments = [],
+    trainers = [];
+
+    password = await bcrypt.hash(password, saltRounds)
+
+    let newGym = {
+        name,
+        username,
+        password,
+        email,
+        address,
+        hours,
+        equipment,
+        classes,
+        trainers,
+        extra,
+        link,
+        reviews,
+        rating,
+        comments,
+    }
+
+
+
+    const gymsCollection = await gyms();
+
+    const insertInfo = await gymsCollection.insertOne(newGym);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId)
+        throw 'Could not add gym';
+
+    return insertInfo;
 }
 
 // Get gym by id
-export const getGymById = async (gymID) => {
-    
+export const getGymById = async (id) => {
+    id = validation.checkId(id);
+    const gymsCollection = await gyms();
+    gym = await gymsCollection.findOne()
+    const gym = await gymsCollection.findOne({_id: new ObjectId(id)});
+    if (!gym) throw 'Gym not found';
+    return gym;
 }
 
 // Update gym content
-export const updateGym = async (gymID) => {
-    
+export const updateGym = async (id, gymObject) => {
+    id = validation.checkId(id);
+    if (!await this.getGymById(id))
+        throw `Gym with ${id} not found`
+    let fields = ["name", "username", "password", "email", "address", "hours", "equipment", "classes", "extra", "link"];
+    Object.keys(gymObject).forEach((field) => {
+        if (!fields.includes(field))
+            throw `gymObject contains an invalid field for updating`
+    })
+    if (gymObject.name)
+        gymObject.name = validation.checkString(gymObject.name)
+    if (gymObject.username)
+        gymObject.username = validation.checkUser(gymObject.username);
+    if (gymObject.password)
+        gymObject.password = validation.checkPassword(gymObject.password)
+    if (gymObject.email)
+        gymObject.email = validation.checkEmail(gymObject.email)
+    if (gymObject.address)
+        gymObject.address = validation.checkPassword(gymObject.address)
+    if (gymObject.hours)
+        gymObject.hours = validation.checkHours(gymObject.hours)
+    if (gymObject.equipment)
+        gymObject.equipment = validation.checkEquipment(gymObject.equipment)
+    if (gymObject.classes)
+        gymObject.classes = validation.checkClasses(gymObject.classes)
+    if (gymObject.extra)
+        gymObject.extra = gymObject.extra.map((ex) => validation.checkString(ex));
+    if (gymObject.link)
+        gymObject.link = validation.checkLink(gymObject.link);
+
+    const gymsCollection = await gyms();
+    const updateInfo = await gymsCollection.findOneAndUpdate(
+      {_id: new ObjectId(id)},
+      {$set: gymObject},
+      {returnDocument: 'after'}
+    );
+    if (!updateInfo) throw 'Update failed';
+
+    return await updateInfo;
+
 }
 
 // Delete gym
 export const deleteGym = async (gymID) => {
-    
+    id = validation.checkId(id);
+    const gymsCollection = await gym();
+    const deletionInfo = await gymsCollection.findOneAndDelete({
+        _id: new ObjectId(id)
+    });
+    if (!deletionInfo) throw `Could not delete gym with id of ${id}`;
+
+    return {_id: id, deleted: true};
 }
--- validation.js
+++ validation.js
@@ -37,23 +37,128 @@ const exportedMethods = {
             throw `You must provide an array of ${varName}`;
     },
 
+    checkObject(obj, objName) {
+        if (typeof obj !== 'object') {
+          throw `${objName} is not an object`;
+        }
+        if (Object.keys(obj).length === 0)
+            throw `${objName} has no fields`
+    },
+
+    checkUser(user) {
+        user = this.checkString(user, "User ID")
+        if (/\d/.test(user))
+            throw `Error: User ID cannot contain digits`
+        if (user.length < 5 || user.length > 10)
+            throw `Error: User ID needs to be at least 5 characters long and must not exceed 25 characters`
+
+        return user.toLowerCase();
+    },
+
     checkPassword(password) {
-        password = this.checkString(password);
-        if (password.length < 8) throw 'password must be at least 8 characters long';
-        if (!/[0-9]/.test(password)) throw 'password must contain at least one number';
-        if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) throw 'password must contain at least one special character';
-        if (/^[0-9!@#$%^&*(),.?":{}|<>]+$/.test(password)) throw 'password cannot be all numbers or special characters';
-        return password;
+        password = this.checkString(password, "Password");
+        if (/\s/.test(password))
+            throw `Error: Password cannot contain spaces`
+
+        if (password.length < 8)
+            throw `Error: Password must be at least 8 characters long`
+
+        if (!/[!@#$%^&*(),.?":{}|<>]/.test(password))
+            throw `Error: Password must contain at least one special character`
+
+        if (!/\d/.test(password))
+            throw `Error: Password must contain at least one digit`
+
+        if (!/[A-Z]/.test(password))
+            throw `Error: Password must contain at least one uppercase alphabetical character`
+
+        return password
     },
 
     checkDOB(dob) { if (!dob || isNaN(Date.parse(dob))) throw 'invalid date of birth' },
 
     checkCommentBody(body) {
-        body = this.checkString(body)
+        body = this.checkString(body, "Comment Body")
         if (body.length > 500)
             throw `Comment must be less than 500 characters long`
         return body;
-    } 
+    },
+
+    checkName(name, varName) {
+        name = this.checkString(name, varName)
+        if (/\d/.test(name))
+            throw `Error: ${varName} cannot contain digits`
+        if (name.length < 2 || name.length > 25)
+            throw `Error: ${varName} needs to be at least 2 characters long and must not exceed 25 characters`
+
+        return name
+    },
+
+    checkHours(hours) {
+        this.checkObject(hours, "Hours");
+        if (Object.keys(hours).length > 7)
+            throw `Invalid Hours: Too many fields supplied`
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        days = days.ForEach((day) => hours[day] = this.checkDay(hours[day]))
+        return hours;
+    },
+
+    checkDay(day) {
+        day = this.checkString(day, "Day");
+        if (!/^(0[1-9]|1[0-2])(am|pm)-0[1-9]|1[0-2](am|pm)$/.test(day) && day !== "Closed")
+            throw `Day's Hours is in invalid format`
+        if (day === "Closed")
+            return day
+        let start = parseInt(day.substring(0, 2)), period1 = day.substring(2, 2), end = parseInt(day.substring(5, 2)), period2 = day.substring(7, 2)
+        if (period1 == period2 && end <= start && day !== "12am-12am")
+            throw `Day's Hours are invalid`
+        return day;
+    },
+
+    checkEmail(email) {
+        email = this.checkString(email, "Email")
+        if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email))
+            throw `Invalid email supplied`
+        return email
+    },
+
+    checkEquipment(equipment) {
+        this.checkArray(equipment);
+        return equipment.map((machine) => {
+            this.checkObject(machine);
+            if (Object.keys(machine).length !== 2)
+                throw `Machine is not valid in equipment`
+
+            machine.type = this.checkString(machine.type, "Machine Type");
+            if (machine.type.length > 50)
+                throw `The length of the type of the machine cannot be more than 50 characters`
+            this.checkNumber(machine.count, "Machine Count")
+            return machine;
+        })
+    },
+
+    checkClasses(classes) {
+        this.checkArray(classes);
+        return classes.map((c) => {
+            this.checkObject(c);
+            if (Object.keys(c).length !== 2)
+                throw `Machine is not valid in equipment`
+            c.name = this.checkString(c.name, "Class Name")
+            if (c.name.length < 2 || c.name.length > 25)
+            throw `Class Name needs to be at least 2 characters long and must not exceed 25 characters`
+            c.description = this.checkString(c.description, "Class Description")
+            if (c.description < 50 ||c.description.length > 500)
+                throw `Class Description must be at least 50 characters and must not exceed 500 characters`
+        })
+    },
+
+    checkLink(link) {
+        link = this.checkString(link, "Link");
+        new URL(link);
+        return link;
+    },
+    
 };
 
 export default exportedMethods;
